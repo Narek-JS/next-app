166bcddea8c5cf4e99cf82d4a8d2a9ad3f97eff8
Narek <narek.petrosyan.js@gmail.com>
2023-03-22 14:12:53 -0700

    unclear issues for vercel

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,5 @@
 import { Global } from '@emotion/react';
-import { globalStyles } from 'shared/globals';
+import { globalStyles } from '../shared/globals';
 
 function MyApp({ Component, pageProps }) {
   return (
